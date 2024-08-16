556e5aa34191e160bbe837ecba80e1db185a7ed4
sungunghwang <eduhsu3@gmail.com>
2024-08-17 01:32:04 +0900

    포켓몬 api 데이터연결,이미지,한글

--- js/script.js
+++ js/script.js
@@ -1,71 +1,73 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //console.log(hash);
+    //가져올 개수
+    const limitNum = 10;
 
-    //===========================================================
+    //=============== 포켓몬 정보 가공하기 =======================================
+    //기본 주소를 이용하여 -> 상세정보를 가져오게되고 -> 그 정보중 일부만 사용하기 위해 다시 loadedPokemon 배열에 넣어둔다.
+    const pokemonList = async (limit = 10) => {
+        try {
+            const apiUrl = `https://pokeapi.co/api/v2/pokemon/?limit=${limitNum}`;
+            const response = await fetch(apiUrl);
+            if (response.ok) {
+                const data = await response.json();
+                //return data.results;
 
-    const pokemonList = (limit = 10, offset = 0) => {
-        const baseUrl = 'https://pokeapi.co/api/v2/pokemon';
-        const queryString = `/?limit=${limit}&offset=${offset}`;
-        fetch(baseUrl + queryString)
-            .then((res) => {
-                console.log(res);
-                if (res.ok) {
-                    return res.json();
-                } else {
-                    return Promise.reject('네트워크 장애');
-                }
-            })
-            .then((resData) => {
-                console.log(resData.results);
-                return resData.results;
-            })
-            .then((itemArr) => {
-                console.log(itemArr[0].url);
-
-                itemArr.map((item) => {
-                    fetch(item.url)
-                        .then((res) => {
-                            console.log(res);
-                            if (res.ok) {
-                                return res.json();
-                            } else {
-                                return Promise.reject('네트워크 장애');
-                            }
-                        })
-                        .then((resData) => {
-                            console.log('최종 : ', resData.results);
-                            // const final = resData.map((item) => {
-                            //     return {
-                            //         id: item.id,
-                            //         name: item.name,
-                            //         image: data.sprites.other['official-artwork'].front_default,
-                            //     };
-                            // });
-                            //console.log(final);
-                            //return final;
-                        })
-                        .catch(() => {});
-                    //return resData.results;
+                const loadedPokemon = data.results.map((item, idx) => {
+                    return {
+                        name: koreanNames[idx],
+                        id: idx + 1,
+                        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${idx + 1}.png`,
+                    };
                 });
-            })
-            .catch(() => {});
-    };
-    const listData = pokemonList(20);
-
-    /* 
-    fetch(baseUrl)
-        .then((res) => {
-            //console.log(res);
-            if (res.ok) {
-                return res.json();
-            } else {
-                return Promise.reject('네트워크 장애');
+                return loadedPokemon;
+            } else if (response.status === 404) {
+                throw new Error('404 에러가 발생했습니다. 서버 관리자에게 요청하세요');
             }
+        } catch (error) {
+            console.error('에러 : ', error);
+        }
+    };
+
+    //========== 화면 렌더링 ===================================
+    const htmlRender = (arr) => {
+        arr.forEach((item, idx) => {
+            const eleBody = document.querySelector('body');
+            let html = '';
+            eleBody.innerHTML += `
+            <p>${item.name}</p>
+            <img src="${item.image}" alt="" />
+            `;
+            /* const eleP = document.createElement('p');
+            eleP.textContent = item.name;
+            eleBody.appendChild(eleP); */
+        });
+    };
+
+    //=========== 한국명 처리 ================================
+    const koreanNames = [];
+    const urls = [];
+    for (let i = 0; i < limitNum; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon-species/${i + 1}/`;
+        urls.push(url);
+    }
+
+    let requestsArr = urls.map((url) => {
+        return fetch(url);
+    });
+
+    Promise.all(requestsArr)
+        .then((response) => {
+            return Promise.all(response.map((res) => res.json()));
         })
-        .then((resData) => {
-            console.log(resData);
-        })
-        .catch((err) => {
-            console.error('에러내용 : ', err);
-        }); */
+        .then((data) => {
+            console.log('어떤값 : ', data);
+            data.forEach((item, idx) => {
+                koreanNames.push(item.names[2].name);
+            });
+
+            //============== 최초 데이터 호출하는 곳은 여기 ===============================
+            pokemonList(10).then((res) => {
+                htmlRender(res);
+            });
+        });
 });
